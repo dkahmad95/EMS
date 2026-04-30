118aed278bc0a08f6ab9eba9ee6940dd91e7eb9f
Ahmad deKmak <dkahmad95@gmail.com>
2026-04-30 14:18:34 +0300

    styling

--- app/Components/DeleteModal.tsx
+++ app/Components/DeleteModal.tsx
@@ -14,10 +14,7 @@ interface ModalProps {
 }
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+ 
   width: 400,
   maxWidth: "90%",
   bgcolor: "white",
@@ -37,29 +34,30 @@ const DeleteModal: React.FC<ModalProps> = ({
 }) => {
   return (
     <div>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style} className="animate-scale-in ">
-          {/* Icon */}
-          <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-danger-100 mb-4 ">
-            <svg
-              className="h-6 w-6 text-danger-600"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-              />
-            </svg>
-          </div>
+     <Modal
+  open={open}
+  onClose={() => setOpen(false)}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+  sx={{
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }}
+>
+  <Box
+    sx={{
+      width: 400,
+      maxWidth: "90%",
+      bgcolor: "white",
+      boxShadow: "0 20px 25px rgba(0,0,0,0.1)",
+      p: 4,
+      borderRadius: "16px",
+      textAlign: "center",
+      border: "1px solid #e2e8f0",
+    }}
+    className="animate-scale-in"
+  >
 
           <Typography
             id="modal-modal-title"
